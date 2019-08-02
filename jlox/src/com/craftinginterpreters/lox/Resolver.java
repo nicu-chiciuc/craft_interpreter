ce779fccea0b6076854ebd2916530ed1708313d2
Nicusor Chiciuc <nicuchiciuc@gmail.com>
2019-08-02 23:05:35 +0300

    Basic class method implementation.

--- jlox/src/com/craftinginterpreters/lox/Resolver.java
+++ jlox/src/com/craftinginterpreters/lox/Resolver.java
@@ -10,7 +10,8 @@ import java.util.Stack;
 public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum FunctionType {
         NONE,
-        FUNCTION
+        FUNCTION,
+        METHOD
     }
 
     private final Interpreter interpreter;
@@ -33,6 +34,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt) {
         declare(stmt.name);
         define(stmt.name);
+
+        for (Stmt.Function method : stmt.methods) {
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
+
         return null;
     }
 
