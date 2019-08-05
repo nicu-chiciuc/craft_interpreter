2a6e5c6f4b0b226c9899d7f60c7a4bf2baed7bea
Nicusor Chiciuc <nicuchiciuc@gmail.com>
2019-08-06 00:59:45 +0300

    Add 'super' keyword.

--- jlox/src/com/craftinginterpreters/lox/Resolver.java
+++ jlox/src/com/craftinginterpreters/lox/Resolver.java
@@ -54,6 +54,11 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
             resolve(stmt.superclass);
         }
 
+        if (stmt.superclass != null) {
+            beginScope();
+            scopes.peek().put("super", true);
+        }
+
         beginScope();
         scopes.peek().put("this", true);
 
@@ -67,6 +72,10 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+
+        if (stmt.superclass != null) endScope();
+
+
         currentClass = enclosingClass;
         return null;
     }
@@ -262,6 +271,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.value);
         resolve(expr.object);
+        return null;
+    }
+
+    @Override
+    public Void visitSuperExpr(Expr.Super expr) {
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
