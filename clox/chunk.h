6fe18f13fb95ae1e1c53339a6c1a52f4314c337a
Nicusor Chiciuc <nicuchiciuc@gmail.com>
2020-12-25 11:59:13 +0200

    Add ValueArray and insert it into Chunk

--- clox/chunk.h
+++ clox/chunk.h
@@ -1,8 +1,8 @@
-
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
   OP_RETURN,
@@ -12,10 +12,12 @@ typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 void freeChunk(Chunk* chunk);
 
 #endif
--- clox/memory.h
+++ clox/memory.h
@@ -1,5 +1,3 @@
-
-
 #ifndef clox_memory_h
 #define clox_memory_h
 
